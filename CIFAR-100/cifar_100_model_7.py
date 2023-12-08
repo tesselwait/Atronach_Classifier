475d61dba46421479442d8baba1217fdb6f89506
Robert Larson <tesselwait@users.noreply.github.com>
2023-12-08 15:36:39 -0500

    Add files via upload

---
+++ CIFAR-100/cifar_100_model_7.py
@@ -0,0 +1,86 @@
+import os, shutil
+import tensorflow_addons as tfa
+import tensorflow as tf
+import keras
+keras.__version__
+import numpy
+import matplotlib.pyplot as plt
+from keras.datasets import cifar100
+from keras.models import Sequential
+from keras.losses import sparse_categorical_crossentropy
+from keras import layers
+from keras import models
+from keras.models import load_model
+from keras.layers import Dense, Dropout, Flatten, BatchNormalization, Activation
+from keras.layers.convolutional import Conv2D, MaxPooling2D
+from keras.constraints import maxnorm
+from keras.callbacks import ModelCheckpoint
+from keras.utils import np_utils
+from keras import optimizers
+from keras.preprocessing import image
+from keras.preprocessing.image import ImageDataGenerator
+from keras.layers import AveragePooling2D
+
+batch_size = 40
+img_width, img_height, img_num_channels = 32, 32, 3
+loss_function = sparse_categorical_crossentropy
+no_classes = 100
+no_epochs = 300
+validation_split = 0.2
+verbosity = 1
+
+(input_train, target_train), (input_test, target_test) = cifar100.load_data()
+input_shape = (img_width, img_height, img_num_channels)
+input_train = input_train.astype('float32')
+input_test = input_test.astype('float32')
+input_train = input_train / 255
+input_test = input_test / 255
+
+model = Sequential()
+model.add(layers.Conv2D(64, (3, 3), activation='relu', input_shape=input_shape))
+model.add(layers.Dropout(0.3))
+model.add(layers.BatchNormalization())
+model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Dropout(0.3))
+model.add(layers.BatchNormalization())
+model.add(layers.Conv2D(256, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Dropout(0.3))
+model.add(layers.BatchNormalization())
+model.add(layers.Conv2D(512, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+
+model.add(layers.Flatten())
+model.add(layers.Dropout(0.3))
+
+model.add(layers.Dense(1024, activation='relu'))
+model.add(layers.Dropout(0.3))
+model.add(layers.BatchNormalization())
+model.add(layers.Dense(512, activation='relu'))
+model.add(layers.Dropout(0.3))
+model.add(layers.BatchNormalization())
+model.add(Dense(100, activation='softmax'))
+
+model.compile(loss=loss_function,
+              optimizer=tfa.optimizers.Lookahead('adam'),
+              metrics=['accuracy'])
+
+history = model.fit(input_train, target_train,
+            batch_size=batch_size,
+            epochs=no_epochs,
+            verbose=verbosity,
+            validation_split=validation_split)
+
+score = model.evaluate(input_test, target_test, verbose=0)
+print(f'Test loss: {score[0]} / Test accuracy: {score[1]}')
+plt.plot(history.history['val_loss'])
+plt.title('Validation loss history')
+plt.ylabel('Loss value')
+plt.xlabel('No. epoch')
+plt.show()
+plt.plot(history.history['val_accuracy'])
+plt.title('Validation accuracy history')
+plt.ylabel('Accuracy value (%)')
+plt.xlabel('No. epoch')
+plt.show()
